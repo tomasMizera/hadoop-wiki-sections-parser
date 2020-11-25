d8b160315878fe2071c5fa77a9ab5b3875c1a509
tomasMizera <tomas.mizera2@gmail.com>
2020-11-25 15:38:24 +0100

    add easy searcher

---
+++ python/src/searcher.py
@@ -0,0 +1,40 @@
+import elasticsearch as EC
+
+
+def search(query, _engine):
+
+    query_body = {
+        "query": {
+            "match": {
+                "sections": query
+            }
+        }
+    }
+
+    return _engine.search(index="sections-i", body=query_body)
+
+
+def process_out(output):
+    print("Found: " + str(output['hits']['total']['value']))
+
+    for out in output['hits']['hits']:
+        print(out['_source']['title'])
+        print(out['_source']['sections'])
+        print()
+
+
+if __name__ == "__main__":
+    _ec = EC.Elasticsearch("localhost:9200")
+    if _ec.ping():
+        print("Connection established to localhost")
+    else:
+        print("Unable to establish connection to elastic")
+
+    while True:
+        text = input("search string: ")
+
+        if text == "exit":
+            exit()
+
+        process_out(search(text, _ec))
+
